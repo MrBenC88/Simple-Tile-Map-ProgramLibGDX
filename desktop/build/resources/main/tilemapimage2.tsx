91b73bd9b9d780ce91b16bed055b116122585c41
Ben C <mrbenc88@students.cs.ubc.ca>
2020-03-26 03:17:58 -0700

    initial commit

---
+++ desktop/build/resources/main/tilemapimage2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="tilemapimage2" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
+ <image source="tilemapimage2.png" width="2048" height="2048"/>
+</tileset>
